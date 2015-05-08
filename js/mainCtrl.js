69e0ab669084c4cc9bd2b3eba210efc0d3bc9982
Vesko Vujovic <veskovujovic@yahoo.com>
2015-05-08 11:32:43 +0200

    fixed validation
    fixed validation  function

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,10 +3,25 @@
     //here we call the same module from app.js
      var app = angular.module('todoApp');
 
-
     //main controller
     var myCtrl = function($scope){
-      $scope.input = 'vesko';
+        //global variables
+        $scope.todo;
+        $scope.state;
+
+        $scope.isEmpty   = function(){
+
+          if(typeof $scope.todo === "undefined" || $scope.todo === null && $scope.todo === "")
+          {
+             $scope.state = true;
+          }
+          else
+          {
+             $scope.state = false;
+          }
+        };
+
+
     };
 
     //registration of this controller
