dc9c3bef6a73ba631f8b700fc9b4a10b20cc963e
Vesko Vujovic <veskovujovic@yahoo.com>
2015-05-11 16:48:45 +0200

    post method works
    post method works

--- js/app1.js
+++ js/app1.js
@@ -3,34 +3,47 @@
  */
 var todoApp = angular.module('todoApp', ['ngCookies']);
 
+//configiration for todoApp
+todoApp.config(['$httpProvider', function($httpProvider){
+
+    //make request visible for laravel
+    $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+}]);
 
 //new services to be used trough app
 todoApp.service('databaseService', function($http){
 
-   //global variables for this class
 
-   //
-    $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest"
 
    //function to post data to database
     this.addToDatabase            = function(input){
 
-        $http.post('/todoback/public/ajax/post/data', 'input=' + input ).success(function(data) {
-            console.log(data);
+        $http.post('/todoback/public/ajax/post/data', {input: input} ).success(function(data) {
+
         });
     };
+
    //function to list data from database
     this.listFromDatabase         = function(){
 
     };
+
    // function to delete one item from database
     this.deleteOneItem            = function(){
 
     };
+
    //function that will delete selected items
     this.deleteSelectedItems      = function(){
 
     };
+
+   //function that will display data
+    this.displayData              = function(data){
+
+    };
+
+
    //some function that will display data after page refreshes
     this.displayDataAfterRefresh  = function(){
 
